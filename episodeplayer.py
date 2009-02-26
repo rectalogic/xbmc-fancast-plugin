646a8c70b3b7b18e9132133eb353a6284d130c7d
Andrew <rectalogic@rectalogic.com>
2009-02-25 22:56:56 -0500

    Build rtmp urls like Hulu plugin does.
    Patch from kreach.
    http://xbmc.org/forum/showpost.php?p=289380&postcount=29

--- episodeplayer.py
+++ episodeplayer.py
@@ -9,13 +9,9 @@ def Main(params):
 
 class EpisodePlayer:
     def __init__(self, videoId, title):
-        videoUrl = self.getVideoUrl(videoId)
-        item = xbmcgui.ListItem(title)
-        item.setProperty("SWFPlayer", "http://www.hulu.com/player.swf")
-        # play won't accept unicode strings, so convert
-        xbmc.Player(xbmc.PLAYER_CORE_DVDPLAYER).play(str(videoUrl), item)
+        self.getVideoUrl(videoId, title)
 
-    def getVideoUrl(self, videoId):
+    def getVideoUrl(self, videoId, title):
         """Get video URL for videoId"""
         try:
             doc = xml.loadXml("http://www.fancast.com/player.widget?videoId=%s" % videoId)
@@ -36,7 +32,49 @@ class EpisodePlayer:
             huluBreak = rtmpUrl.find('<break>')
             if (huluBreak >= 0):
                 # Hulu style rtmp URL
-                return rtmpUrl[0:huluBreak]
+                #code to determine proper streaming url from
+                #http://code.google.com/p/xbmc-addons/source/browse/trunk/plugins/video/Hulu/resources/lib/stream_hulu.py#75
+                rtmpUrl=rtmpUrl.replace('&amp;','&').replace('&lt;','<').replace('&gt;','>')
+                mainParts = rtmpUrl.split("?")
+                queryStringParts = mainParts[1].split("&")
+                v9 = queryStringParts[0]
+                v6 = queryStringParts[1]
+
+                if "<break>" in queryStringParts[2]:
+                    breakParts = queryStringParts[2].split("<break>")
+                    v3 = breakParts[0];
+                    fileName = breakParts[1]
+                else:
+                    v3 = queryStringParts[2]
+                    breakFilenameURL = mainParts[0].split("://");
+                    breakFilenamedir = breakFilenameURL[1].split("/");
+                    breakFilenames = breakFilenameURL[1].split(breakFilenamedir[1] + "/")
+                    fileName = breakFilenames[1]
+
+                newQueryString = v9 + "&" + v6 + "&" + v3
+
+                protocolSplit = rtmpUrl.split("://")
+                pathSplit = protocolSplit[1].split("/");
+                serverPath = pathSplit[0] + "/" + pathSplit[1];
+                server = pathSplit[0];
+                appName = pathSplit[1];
+                videoIdentIp = server
+
+                protocol = "rtmp";
+                port = "1935";
+                newUrl =  protocol + "://" + videoIdentIp + ":" + port + "/" + appName + "?_fcs_vhost=" + server
+                if newQueryString <> "":
+                    newUrl += "&" + newQueryString
+                SWFPlayer = 'http://www.hulu.com/player.swf'
+                item = xbmcgui.ListItem(title)
+                item.setProperty("SWFPlayer", SWFPlayer)
+                item.setProperty("PlayPath", fileName)
+                item.setProperty("PageURL", "http://www.fancast.com/player.widget?videoId=%s" % videoId)
+                playlist = xbmc.PlayList(1)
+                playlist.clear()
+                playlist.add(newUrl, item)
+                play=xbmc.Player().play(playlist)
+                xbmc.executebuiltin('XBMC.ActivateWindow(fullscreenvideo)')
             else:
                 # Fancast style rtmp URL
                 # Need to split application and filename out of rtmp url:
@@ -44,6 +82,10 @@ class EpisodePlayer:
                 # Changes to:
                 #   rtmp://cp40359.edgefcs.net/ondemand?slist=fancast/Comcast_CIM_Prod_Fancast_Partner/836/252/COMEDYCENTRAL_COLBERTREPORT_4125
                 match = re.compile('^(.*ondemand)/(.*).flv$').match(rtmpUrl)
-                return "%s?slist=%s" % (match.group(1), match.group(2))
+                videoUrl = "%s?slist=%s" % (match.group(1), match.group(2))
+                item = xbmcgui.ListItem(title)
+                item.setProperty("SWFPlayer", "http://www.hulu.com/player.swf")
+                # play won't accept unicode strings, so convert
+                xbmc.Player(xbmc.PLAYER_CORE_DVDPLAYER).play(str(videoUrl), item)
         finally:
             if (doc): doc.unlink()
