a8c44e1096d7532ce92d42f1d93b82dbb13b4368
isobsr <hahaha@gmail.com>
2009-02-28 17:55:47 -0700

    1. format file dates to DD-MM-YYYY to fix sorting by date 2. use "Plot" infoLabel for description -- view by pressing the white button and clicking "Movie Information"

--- episodelist.py
+++ episodelist.py
@@ -2,6 +2,7 @@ import sys
 import urllib
 import xbmcplugin, xbmcgui
 import xmlhelper as xml
+import time
 
 def Main(params):
     EpisodeList(**params)
@@ -53,6 +54,14 @@ class EpisodeList:
                     pubDate = xml.getChildData(item, cimNS, 'origAirDate', isRequired=False)
                     if (not pubDate):
                         pubDate = xml.getChildData(item, None, 'pubDate', isRequired=False)
+                    if (pubDate):
+                        try:
+                            # convert date to DD-MM-YYYY so sorting by date works correctly.
+                            # I found this date format in the Apple Movie Trailers II plugin
+                            sdate = time.strptime(pubDate[5:16], "%d %b %Y")
+                            pubDate = "%02d-%02d-%04d" % (sdate.tm_mday, sdate.tm_mon, sdate.tm_year)
+                        except:
+                            pass
                     description = xml.getChildData(item, None, 'description', isRequired=False)
                     url = sys.argv[0] + "?" + urllib.urlencode({ 'module': 'episodeplayer',
                                                                  'videoId': xml.getChildData(item, cimNS, 'videoId'),
@@ -60,7 +69,7 @@ class EpisodeList:
                     item = xbmcgui.ListItem(title, iconImage="DefaultVideo.png", thumbnailImage=thumbnail)
                     item.setInfo(type="Video",
                                  infoLabels={ "Title": title,
-                                              "Description": description,
+                                              "Plot": description,
                                               "Date": pubDate })
                     if not xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]), url=url,
                                                        listitem=item, totalItems=totalItems):
